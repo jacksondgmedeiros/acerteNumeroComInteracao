731d843f1664cb46ad1d9ebe7b3539131df62059
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-10-18 23:17:44 -0300

    adicionando os parametros para não repetir o número já sorteado após clicar em novo jogo, armazenando os números sorteados em uma lista, deixando o número limite dinâmico para não alterar o código em vários lugares e como extra, foi implementado a leitura do texto inicial com uma API nativa para os principais navegadores

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
+let listaDeNumerosAleatorios = [];
+let numeroLimite = 4;
 let tentativas = 1;
 let numeroSecreto = gerarNumeroAleatorio();
 
 exibirMensagemInicial();
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosAleatorios.length;
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosAleatorios = [];
+    }
+
+    if(listaDeNumerosAleatorios.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else{
+        listaDeNumerosAleatorios.push(numeroEscolhido);
+        console.log(listaDeNumerosAleatorios);
+        return numeroEscolhido;
+    }
 }
 
 function exibirNomeNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto.toString();
+
+    // foi adicionado ao código, a fala do código, no html tem o script para o site
+    //responsiveVoice.speak(texto, 'Portuguese Female', {rate:1.2});
+    // já o código abaixo, é nativo para os navegadores principais
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 function exibirMensagemInicial() {
     exibirNomeNaTela('h1', 'Jogo do número secreto');
-    exibirNomeNaTela('p', 'Escolha um número entre 1 a 10');
+    exibirNomeNaTela('p', 'Escolha um número entre 1 a ' + numeroLimite);
 }
 
 function verificarChute() {
